4a3af2c2d176fe51313e295f35186f8d3356565a
sahil2019 <sahil2011.raj@gmail.com>
2021-03-09 23:59:52 +0530

    bikes:v2

--- front_end/royal-bros/src/App.js
+++ front_end/royal-bros/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {LeftDrawer} from './Components/leftDrawer/App/LeftDrawer';
 import React from "react";
 import Tarrif from "./Components/Tarrif/Tarrif";
-
+import Bikes from "./Components/Bikes/Bikes"
 function App() {
   return (
     <div className="App">
-      <LeftDrawer/>
-      <Tarrif></Tarrif>
+      {/* <LeftDrawer/>
+      <Tarrif></Tarrif> */}
+    <Bikes/>
     </div>
   );
 }
