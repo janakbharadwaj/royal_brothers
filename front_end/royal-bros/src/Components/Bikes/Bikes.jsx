5988058a9d89cca9ec4e256bb49f68a860836f3d
sahil2019 <sahil2011.raj@gmail.com>
2021-03-13 01:53:04 +0530

    bikes:sort

--- front_end/royal-bros/src/Components/Bikes/Bikes.jsx
+++ front_end/royal-bros/src/Components/Bikes/Bikes.jsx
@@ -7,7 +7,9 @@ import BikesFiter from "./BikesFilter";
 import { SelectionContext } from "../../Context/SelectionContextProvider";
 import TimeConversion from "./utils"
 import { tarrifReducer } from "../../Redux/Tarrif/TarrifReducer";
+import BikeItem from "./BikeItem";
 const Bikes = () => {
+  const[sortData,setSortData]=React.useState([])
   const dispatch = useDispatch();
   const history = useHistory();
   const isLoading = useSelector((state) => state.bikes.isLoading);
@@ -17,8 +19,6 @@ const Bikes = () => {
   const { pickupDate, pickupTime, dropDate, dropTime } = info;
   const relevantData=useSelector(state=>state.bikesFilter.relevantData)
   const currBikeLoc = useSelector((state) => state.tarrifReducer.currentLocation);
-  
-  console.log(currBikeLoc._id)
   const [dateTimings, setDateTimings] = React.useState({
     pickUpDate: pickupDate,
     pickUpTime: pickupTime,
@@ -42,12 +42,28 @@ const Bikes = () => {
   };
    const { pickUpDate, pickUpTime, dropOffDate, dropOffTime } = dateTimings;
   const[actRele,setRele]=React.useState(true)
+
+   const highToLow=()=>{
+    let newList = bikesData.sort((a,b) => (a.hourly_rate > b.hourly_rate) ? -1 : 
+    ((b.hourly_rate > a.hourly_rate) ? 1 : 0))
+    setSortData([...newList])
+   }
+   const lowToHigh=()=>{
+    let newList = bikesData.sort((a,b) => (a.hourly_rate > b.hourly_rate) ? 1 : 
+    ((b.hourly_rate > a.hourly_rate) ? -1 : 0))
+    setSortData([...newList])
+   }
+  const relevancefunc=()=>{
+    setSortData([])
+    
+  }
+
   return (
     <>
     <div className={styles.upper__cont}>
-      {actRele?<span style={{fontWeight:"bolder",borderBottom:"4px solid yellow"}}>Relevance</span>:<span>Relevance</span>}
-      <span>highToLow</span>
-      <span>Low to high</span>
+     <button onClick={relevancefunc}>Relevance</button>
+      <button onClick={highToLow} >Price-High to Low</button>
+      <button onClick={lowToHigh}>Price-Low to High</button>
     </div>
     <div className={styles.main__cont}>
       <div className={styles.main__cont__filters}>
@@ -63,90 +79,12 @@ const Bikes = () => {
       <div className={styles.main__cont__bikesdata}>
         <div className={styles.main__cont__bikesdata__Items}>
             {relevantData?.map((bike)=>
-                                <div key={bike._id}>
-                                  <div>
-                                    <h4>{bike.bike_name}</h4>
-                                    <img src={bike.bike_image} alt="bike" />
-                                  </div>
-                                  <div className={styles.bikeItems__availability}>
-                                    <p>Available at</p>
-                                    <select>
-                                      {["hello1", "hello2"].map((tag) => (
-                                        <option key={tag} value={tag}>
-                                          {tag}
-                                        </option>
-                                      ))}
-                                    </select>
-                                  </div>
-                                  <div className={styles.bikeItems__timings}>
-                                    <div>
-                                      <p>{pickUpTime}</p>
-                                      <p>{pickUpDate}</p>
-                                    </div>
-                                    <div>to</div>
-                                    <div>
-                                      <p>{dropOffTime}</p>
-                                      <p>{dropOffDate}</p>
-                                    </div>
-                                  </div>
-                                  <div className={styles.bikeItem__booking}>
-                                    <div>
-                                      <p>
-                                     
-                                        &#x20b9; {(pickUpDate && dropOffDate)  && Number(TimeConversion(pickUpDate,dropOffDate))*24*bike.hourly_rate}
-                                        <br />
-                                        ( km included)
-                                      </p>
-                                    </div>
-                                    <div>
-                                      <button onClick={()=>handleBooking(bike._id)}>BOOK</button>
-                                    </div>
-                                  </div>
-                              </div>
-            
+                <BikeItem {...bike} pickUpDate={pickUpDate} pickUpTime={pickUpTime} dropOffDate={dropOffDate} dropOffTime={dropOffTime} handleBooking={handleBooking}/>
             )}
             {selectedBike.bike_name?
-            <div key={selectedBike._id}>
-                                  <div>
-                                    <h4>{selectedBike.bike_name}</h4>
-                                    <img src={selectedBike.bike_image} alt="bike" />
-                                  </div>
-                                  <div className={styles.bikeItems__availability}>
-                                    <p>Available at</p>
-                                    <select>
-                                      {["IndiraNagar", "koramangala"].map((tag) => (
-                                        <option key={tag} value={tag}>
-                                          {tag}
-                                        </option>
-                                      ))}
-                                    </select>
-                                  </div>
-                                  <div className={styles.bikeItems__timings}>
-                                    <div>
-                                      <p>{pickUpTime}</p>
-                                      <p>{pickUpDate}</p>
-                                    </div>
-                                    <div>to</div>
-                                    <div>
-                                      <p>{dropOffTime}</p>
-                                      <p>{dropOffDate}</p>
-                                    </div>
-                                  </div>
-                                  <div className={styles.bikeItem__booking}>
-                                    <div>
-                                      <p>
-                                     
-                                        &#x20b9; {(pickUpDate && dropOffDate)  && Number(TimeConversion(pickUpDate,dropOffDate))*24*selectedBike.hourly_rate}
-                                        <br />
-                                        ( km included)
-                                      </p>
-                                    </div>
-                                    <div>
-                                      <button onClick={()=>handleBooking(selectedBike._id)}>BOOK</button>
-                                    </div>
-                                  </div>
-                              </div>
-                          :" "}
+            
+            <BikeItem {...selectedBike} pickUpDate={pickUpDate} pickUpTime={pickUpTime} dropOffDate={dropOffDate} dropOffTime={dropOffTime} handleBooking={handleBooking}/>
+            :" "}
 
 
 
@@ -157,49 +95,14 @@ const Bikes = () => {
             <div>something went wrong</div>
           ) : isLoading ? (
             <div>....loading</div>
-          ) : (
+          ) :sortData.length==0?(
             bikesData.map((bike) => (
-              <div key={bike._id}>
-                <div>
-                  <h4>{bike.bike_name}</h4>
-                  <img src={bike.bike_image} alt="bike" />
-                </div>
-                <div className={styles.bikeItems__availability}>
-                  <p>Available at</p>
-                  <select>
-                    {["IndiraNagar", "koramangala"].map((tag) => (
-                      <option key={tag} value={tag}>
-                        {tag}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className={styles.bikeItems__timings}>
-                  <div>
-                    <p>{pickUpTime}</p>
-                    <p>{pickUpDate}</p>
-                  </div>
-                  <div>to</div>
-                  <div>
-                    <p>{dropOffTime}</p>
-                    <p>{dropOffDate}</p>
-                  </div>
-                </div>
-                <div className={styles.bikeItem__booking}>
-                  <div>
-                    <p>
-          
-                      &#x20b9;{(pickUpDate && dropOffDate)  && Number(TimeConversion(pickUpDate,dropOffDate))*24*bike.hourly_rate}
-                      <br />
-                      ( km included)
-                    </p>
-                  </div>
-                  <div>
-                    <button onClick={()=>handleBooking(bike._id)}>BOOK</button>
-                  </div>
-                </div>
-              </div>
+             
+              <BikeItem {...bike} pickUpDate={pickUpDate} pickUpTime={pickUpTime} dropOffDate={dropOffDate} dropOffTime={dropOffTime} handleBooking={handleBooking}/>
             ))
+          ):(
+            sortData.map((bike)=>
+            <BikeItem {...bike} pickUpDate={pickUpDate} pickUpTime={pickUpTime} dropOffDate={dropOffDate} dropOffTime={dropOffTime} handleBooking={handleBooking}></BikeItem>)
           )}
         </div>
       </div>
