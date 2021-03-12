6dcc6efd7b52fdf24d8164e614861006afe3cdbd
janakbharadwaj <janakbharadwaj.adluri@gmail.com>
2021-03-12 15:31:56 +0530

    .

--- front_end/royal-bros/src/Components/RoyalBrosX/RoyalBrosIndBike.jsx
+++ front_end/royal-bros/src/Components/RoyalBrosX/RoyalBrosIndBike.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 //import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import axios from 'axios'
+import styles from './indBike.module.css'
 
 const RoyalBrosIndBike = () => {
     const[month,setMonth] = React.useState(0)
@@ -9,6 +10,18 @@ const RoyalBrosIndBike = () => {
     const[deposit,setDeposit] = React.useState(0)
     const [indBike,setIndBike] = React.useState([])
     const { id } = useParams();
+    const[date,setDate]=React.useState('')
+    const[time,setTime]=React.useState('')
+    const[discount,setDiscount] = React.useState(0)
+    const[total,setTotal] = React.useState(0)
+
+    React.useEffect(()=>{
+        let abc = localStorage.getItem("dateTime")
+        let dateTime = JSON.parse(abc)
+        //console.log(dateTime)
+        setDate(dateTime.date)
+        setTime(dateTime.time)
+    },[])
     
     React.useEffect(()=>{
         axios.get(`http://localhost:8080/bikes/${id}`)
@@ -26,22 +39,43 @@ const RoyalBrosIndBike = () => {
         }else{
             setDeposit(1400)
         }
+
+        if(mnth<=2){
+            setDiscount(5)
+        }
+        if(mnth>2 && mnth<=6){
+            setDiscount(10)
+        }
+        if(mnth>6){
+            setDiscount(15)
+        }
     }
 
+    let disc = rate*discount/100
+    console.log(rate-disc)
+
     return (
-        <div>
-            <div>
+        <div className={styles.indBikeParentDiv}>
+            <div className={styles.bikeDetails}>
                 <img src={indBike.bike_image} alt=''/>
                 <h2>{indBike.bike_name}</h2>
-                <p>Km Limit {indBike.kilometer_limit}</p>
-                <p>Hourly rate {indBike.hourly_rate}</p>
+                <h3>Km Limit : {indBike.kilometer_limit} km/month</h3>
+                <h3>Hourly rate : <i class="fas fa-rupee-sign"> </i> {indBike.hourly_rate} rupees/km</h3>
             </div>
-            <div>
-                <h2>subscription details</h2>
-                <input type='number' onChange={(e)=>setMonth(e.target.value)} value={month}/>
-                <button onClick={validateRate}>validate</button>
-                <h2>{`rate for ${month}months is ${rate}`}</h2>
-                <h2>deposit {deposit}</h2>
+            <div className={styles.subsDetails}>
+                <h2>Subscription Details</h2>
+                <h3>Start from <h4 style={{color:"rgb(254,210,80)",fontWeight:"bold"}}> <i class="fas fa-table"> </i> :- {date}  <span> <i class="fas fa-grip-lines-vertical"> </i> </span> <i class="far fa-clock"> </i> :- {time} </h4></h3>
+                <br/>
+                <label for='mnth'>Enter no of Months</label><br/>
+                <input type='' id='mnth' onChange={(e)=>setMonth(e.target.value)} value={month}/><br/>
+                <button onClick={validateRate}>validate</button><br/>
+                <div className={styles.ratesDiv}>
+                    <p>Rent per Month : <span>{(indBike.hourly_rate)*24*30} <i class="fas fa-rupee-sign"> </i> </span></p>
+                    <p>Rent for choosen months: {rate?<span>{rate} <i class="fas fa-rupee-sign"> </i> </span>:<span>0</span>}</p>
+                    <p>Mandatory Deposit : <span>{deposit} <i class="fas fa-rupee-sign"> </i> </span></p>
+                    <p>Discount : <span>{discount}%</span></p>
+                    <h3>Total : <span>{rate - disc} <i class="fas fa-rupee-sign"> </i> </span></h3>
+                </div>
             </div>
         </div>
     )
